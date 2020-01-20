01a29cd21a492821e8ffa94b0241b26788668df4
TimScheifler <tim.scheifler@web.de>
2020-01-20 14:38:48 +0000

    - added log4J to pom.xml - changed sout to LOG.info for console output

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,8 +1,13 @@
 package com.napier.sem;
 
+import org.apache.log4j.spi.LoggerFactory;
+
+import java.util.logging.Logger;
+
 public class App {
 
+    private final static Logger LOG = Logger.getLogger(String.valueOf(App.class));
     public static void main(String[] args){
-        System.out.println("Boo yah!");
+        LOG.info("Boo yah!");
     }
 }
