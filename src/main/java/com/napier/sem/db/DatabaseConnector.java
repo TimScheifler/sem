ab25bc5b6df9a56b348c48aa3de1a65cc3be10fe
Tim Scheifler <40459029@live.napier.ac.uk>
2020-02-02 14:50:20 +0000

    - Created a Pojo Class "Employee". - Added getEmployee to DatabaseConnector, which return an Employee with a specific ID. - Added a Logger to DatabaseConnector.

--- src/main/java/com/napier/sem/db/DatabaseConnector.java
+++ src/main/java/com/napier/sem/db/DatabaseConnector.java
@@ -1,10 +1,13 @@
 package com.napier.sem.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.napier.sem.Employee;
+
+import java.sql.*;
+import java.util.logging.Logger;
 
 public class DatabaseConnector {
+
+    private final static Logger LOG = Logger.getLogger(String.valueOf(DatabaseConnector.class));
 
     /**
      * Connection to MySQL database.
@@ -53,6 +56,39 @@ public class DatabaseConnector {
             } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public Employee getEmployee(int ID){
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
         }
     }
 }
